1d39cd3fecf2ec23455c4e0af5249c62f0406a7a
namagaii <rnoel2125@gmail.com>
2021-11-30 23:39:29 -0500

    Finsihed implementing history buttons and uvi coloring and added styling (w.i.p.)

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-let requestURL = "http://api.openweathermap.org/data/2.5/weather?appid=f310e487a963435a12a4d57a94cd2dff"; //root
+const REQUESTROOT = "http://api.openweathermap.org/data/2.5/weather?appid=f310e487a963435a12a4d57a94cd2dff"; //root
 let weatherData;
 const SEARCHHISTORYKEY = "NR-WDBSearchHistoryData";
 let searchHistoryIndex = 0;
@@ -10,13 +10,15 @@ $("#Search-Form").on("submit", event=>{
     UpdateSearchHistory($("#Search-Bar").val());
 });
 
-$("#Search-History").on("submit", event=>{
+$("#Search-History").on("click", event=>{
+    console.log(event.target.textContent);
     if (event.target.tagName === "BUTTON"){
         FetchLocation(event.target.textContent);
     }
 })
 
 function FetchLocation(p_cityName){
+    let requestURL = REQUESTROOT;
     requestURL += "&q=" + p_cityName;
     fetch(requestURL)
     .then(function(response){
@@ -54,11 +56,11 @@ function DisplayTodaysData(p_weatherData){
     _todaysDisplay.children("p").eq(1).children("span").text(p_weatherData.wind_speed);
     _todaysDisplay.children("p").eq(2).children("span").text(p_weatherData.humidity);
     _todaysDisplay.children("p").eq(3).children("mark").text(p_weatherData.uvi);
-    //_todaysDisplay.children("p").eq(3).children("mark").attr("class", GetUVColor(p_weatherData.uvi));
+    _todaysDisplay.children("p").eq(3).children("mark").attr("id", GetUVColor(p_weatherData.uvi));
 }
 
 function DisplayFiveDayData(p_weatherData, p_index){
-    let _todaysDisplay = $("#5-Day-Forecast").children("#Day-"+p_index);
+    let _todaysDisplay = $("#Five-Day-Forecast").children("#Day-"+p_index);
     _todaysDisplay.children("time").text(moment.unix(p_weatherData.dt).format('MM-DD-YYYY'));
     _todaysDisplay.children("figure").children("img").attr("src", "https://openweathermap.org/img/wn/" + p_weatherData.weather[0].icon + ".png");
     _todaysDisplay.children("p").eq(0).children("span").text(p_weatherData.temp.max);
@@ -66,7 +68,19 @@ function DisplayFiveDayData(p_weatherData, p_index){
     _todaysDisplay.children("p").eq(2).children("span").text(p_weatherData.humidity);
 }
 
-// function GetUVColor() {}
+function GetUVColor(p_uvi) {
+    if (p_uvi >= 11){
+        return "Extreme"
+    } else if (p_uvi >= 8) {
+        return "Very-High"
+    } else if (p_uvi >= 6) {
+        return "High"
+    } else if (p_uvi >= 3) {
+        return "Moderate"
+    } else {
+        return "Low"
+    }
+}
 
 function DisplaySearchHistory(){
     if (!localStorage.getItem(SEARCHHISTORYKEY)) { return; }
@@ -87,25 +101,23 @@ function UpdateSearchHistory(query){
     if (localStorage.getItem(SEARCHHISTORYKEY)){
         _wasPopulated = true;
         let _searchHistory = JSON.parse(localStorage.getItem(SEARCHHISTORYKEY));
-        for (let i = 0; i < _searchHistory.queries.numResults; i++){
+        for (let i = 0; i < _searchHistory.numResults; i++){
             if (_searchHistory.queries[i] === query){
                 console.log("Item already saved.")
                 return;
             }
         }
     }
-    // Otherwise save it
-    else{
-        let _newSearchHistory = {
-            numResults: 0,
-            queries: []
-        }
-        if(_wasPopulated){
-            _newSearchHistory.queries = JSON.parse(localStorage.getItem(SEARCHHISTORYKEY)).queries;
-            _newSearchHistory.numResults = _newSearchHistory.queries.push(query);
-        } else {
-            _newSearchHistory.numResults = _newSearchHistory.queries.push(query);
-        }
-        localStorage.setItem(SEARCHHISTORYKEY, JSON.stringify(_newSearchHistory));
+    // Otherwise save it 
+    let _newSearchHistory = {
+        numResults: 0,
+        queries: []
     }
+    if(_wasPopulated){
+        _newSearchHistory.queries = JSON.parse(localStorage.getItem(SEARCHHISTORYKEY)).queries;
+        _newSearchHistory.numResults = _newSearchHistory.queries.push(query);
+    } else {
+        _newSearchHistory.numResults = _newSearchHistory.queries.push(query);
+    }
+    localStorage.setItem(SEARCHHISTORYKEY, JSON.stringify(_newSearchHistory));
 }
